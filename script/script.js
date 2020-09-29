c1e23eb27f51d66451c2e5e9c40ef9d0622dd472
Victor Ângelo Sánchez da Silva <53021597+victorangelov@users.noreply.github.com>
2020-09-28 22:40:03 -0300

    em andamento
    em andamento

--- script/script.js
+++ script/script.js
@@ -1,1 +1,28 @@
-//alert('Olá! mather fucker')
+let num = window.document.querySelector('input#txtnum')
+let lista = window.document.querySelector('select#flista')
+let res = window.document.querySelector('div#res')
+let valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n,l){
+    if(l.indexOf(Number(n) != -1)){
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        window.alert(`Tudo ok`)
+    }else{
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+}
